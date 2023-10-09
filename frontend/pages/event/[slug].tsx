b0b5501a0afd9149006afe9e8970b689d6d5b373
kaf-lamed-beyt <belac335@gmail.com>
2023-10-09 14:37:05 +0100

    chore: add dynamic route for events

--- frontend/pages/event/[slug].tsx
+++ frontend/pages/event/[slug].tsx
@@ -1,50 +1,19 @@
-// import { Box, Text } from "@chakra-ui/react";
-// import { getEvents } from "@utils/fetcher";
-// import { GetStaticPropsContext } from "next";
-// import React from "react";
-// import fetch from "node-fetch";
-// import Head from "next/head";
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import Head from "next/head";
 
-// export async function getStaticPaths() {
-//   const request = await fetch("https://ng-tech-events.netlify.app/event");
-//   const response = await request.json();
+const EventDetails = () => {
+  return (
+    <>
+      <Head>
+        <title>Event name &mdash; Techies Assemble</title>
+      </Head>
 
-//   const events = response?.events;
+      <Box>
+        <Text fontSize="lg">Welcome to this unique page</Text>
+      </Box>
+    </>
+  );
+};
 
-//   console.log(events);
-
-//   const paths = events.map(({ slug }) => ({
-//     params: {
-//       slug: slug,
-//     },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
-// export async function getStaticProps(context: GetStaticPropsContext) {
-//   return {
-//     props: {
-//       data: {},
-//     },
-//   };
-// }
-
-// const EventDetails = () => {
-//   return (
-//     <>
-//       <Head>
-//         <title>Event name &mdash; Techies Assemble</title>
-//       </Head>
-
-//       <Box>
-//         <Text fontSize="lg">Welcome to this unique page</Text>
-//       </Box>
-//     </>
-//   );
-// };
-
-// export default EventDetails;
+export default EventDetails;
